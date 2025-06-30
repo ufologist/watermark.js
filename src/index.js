a4b5c2332e2e7fad9d572e9dbf28079e7d27ffdc
Sun <klsforever@gmail.com>
2025-06-30 15:59:16 +0800

    fix: 由于元素高度获取不到导致水印没有绘制出来
    在 iOS 16.1.1 测试时 getComputedStyle(span).height 获取到的值为 'auto'
    导致 textHeight 得出 NaN, 进而无法绘制出水印
    因此读取一下布局属性(如 offsetHeight、offsetWidth、clientHeight、clientWidth 等)
    强制浏览器计算当前布局, 让 getComputedStyle(span).height 获取到正确值

--- src/index.js
+++ src/index.js
@@ -132,7 +132,13 @@ Watermark.prototype._getTextHeight = function(text, font) {
     span.appendChild(document.createTextNode(text));
 
     document.body.appendChild(span);
-    var textHeight = parseInt(getComputedStyle(span).height);
+    // fix: 由于元素高度获取不到导致水印没有绘制出来
+    // 在 iOS 16.1.1 测试时 getComputedStyle(span).height 获取到的值为 'auto'
+    // 导致 textHeight 得出 NaN, 进而无法绘制出水印
+    // 因此读取一下布局属性(如 offsetHeight、offsetWidth、clientHeight、clientWidth 等)
+    // 强制浏览器计算当前布局, 让 getComputedStyle(span).height 获取到正确值
+    var textHeight = span.offsetHeight;
+    textHeight = parseInt(getComputedStyle(span).height);
     document.body.removeChild(span);
 
     return textHeight;
